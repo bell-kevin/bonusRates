cd0478a4a44190306bbce949fcfc710bd4413942
Kevin Bell <bellKevin@pm.me>
2022-05-06 03:00:31 +0000

    Update BonusRates.java

--- bonusRates/src/bonusrates/BonusRates.java
+++ bonusRates/src/bonusrates/BonusRates.java
@@ -17,22 +17,21 @@ public class BonusRates {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.print("Bonus Rates by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         int yearsOfService;
         int salesTotal;
-        int bonusRate = 0;
+        String bonusRate;
         System.out.print("Enter employee's years of service: ");
         yearsOfService = computerKeyboardInput.nextInt();
         System.out.print("Enter employee's sales total: ");
         salesTotal = computerKeyboardInput.nextInt();
         if (yearsOfService > 10 || salesTotal > 250000) {
-            bonusRate = 2;
+            bonusRate = "2%";
         } else if (yearsOfService > 5 || salesTotal > 125000) {
-            bonusRate = 1;
+            bonusRate = "1%";
         } else {
-            bonusRate = 100;
+            bonusRate = "$100";
         }
         System.out.print("bonus rate = " + bonusRate + "\n");
     }
